0d46873528227ebf689b944deff4cbda32dcc2a6
Harshit Agrawal <noreply@example.com>
2025-02-26 23:04:06 +0530

    Designed and Implemented the UI of the project

---
+++ app/layout.js
@@ -0,0 +1,38 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Buy Me A Chai",
+  description: "This website is a crowdfunding platform for chai lovers",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Navbar />
+
+        <div className="min-h-[80vh] bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]">
+
+          {children}
+        </div>
+        
+        <Footer />
+      </body>
+    </html>
+  );
+}
