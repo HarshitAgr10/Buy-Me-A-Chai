ba815eab515d57685e637ee58c104ee7bb279b8f
Harshit Agrawal <noreply@example.com>
2025-02-27 18:38:17 +0530

    Designed the creator's page with payment options and notifications

--- components/Navbar.js
+++ components/Navbar.js
@@ -17,10 +17,10 @@ const Navbar = () => {
 
     return (
         <nav className='bg-gray-900 text-white flex justify-between items-center px-4 h-16'>
-                <Link className="logo font-bold text-lg flex justify-center items-center" href={"/"}>
-                    <img src="tea.gif" width={44} alt="" />
-                    <span>Buy Me A Chai</span>
-                </Link>
+            <Link className="logo font-bold text-lg flex justify-center items-center" href={"/"}>
+                <img src="tea.gif" width={44} alt="" />
+                <span>Buy Me A Chai</span>
+            </Link>
             {/* <ul className='flex justify-between gap-4'>
                 <li>Home</li>
                 <li>About</li>
@@ -31,11 +31,11 @@ const Navbar = () => {
 
             <div className='relative'>
 
-                {session && <><button 
-                onClick={() => setShowDropdown(!showDropdown)} 
-                onBlur={()=> {setTimeout(() => {setShowDropdown(false)}, 100); }} id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white mx-4 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Welcome {session.user.email}<svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
-                </svg>
+                {session && <><button
+                    onClick={() => setShowDropdown(!showDropdown)}
+                    onBlur={() => { setTimeout(() => { setShowDropdown(false) }, 100); }} id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white mx-4 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Welcome {session.user.email}<svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
+                    </svg>
                 </button>
 
 
